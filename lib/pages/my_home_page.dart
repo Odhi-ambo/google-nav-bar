720f294a6eef66211219677637b2082e18a0f10b
Odhi-ambo <michaelderrick605@gmail.com>
2024-11-19 16:01:39 +0300

    UI

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -12,34 +12,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: GNav(
-          gap: 8,
-          activeColor: Colors.white,
-          iconSize: 24,
-          backgroundColor: Colors.black,
-          color: Colors.amberAccent,
-          tabBackgroundColor: Colors.grey.shade800,
-          padding: const EdgeInsets.all(16),
-          tabs: const [
-            GButton(
-              icon: Icons.home,
-              text: 'Home',
-            ),
-            GButton(
-              icon: Icons.search,
-              text: 'Search',
-            ),
-            GButton(
-              icon: Icons.favorite,
-              text: 'Favourite',
-            ),
-            GButton(
-              icon: Icons.person,
-              text: 'Profile',
-            ),
-          ],
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+            gap: 8,
+            activeColor: Colors.white,
+            iconSize: 24,
+            backgroundColor: Colors.black,
+            color: Colors.amberAccent,
+            tabBackgroundColor: Colors.grey.shade800,
+            padding: const EdgeInsets.all(16),
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+              ),
+              GButton(
+                icon: Icons.favorite,
+                text: 'Favourite',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
+          ),
         ),
       ),
     );
